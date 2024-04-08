969c517338577d27c4e76a41d073a8f20b365511
Andrew Murray <andrew@anamnesisgames.com>
2024-04-08 16:47:35 -0400

    delete KiwiTasker
    I need to do an audit of this code before I use it for a project

--- libs/KiwiTasker/include/kiwi/KIWI_ThreadImpl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined(KIWI_USE_WINTHREAD_IMPL)
-#include "kiwi/KIWI_ThreadImplWinThread.h"
-#elif defined(KIWI_USE_PTHREAD_IMPL)
-#include "kiwi/KIWI_ThreadImplPthread.h"
-#else
-#error This platform has no thread implementation KiwiTasker
-#endif
