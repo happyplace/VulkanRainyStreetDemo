9e60bb15e7bb4f6758a26e3f3ad8a7a6351249cf
Andrew Murray <andrew@anamnesisgames.com>
2024-06-19 23:33:24 -0400

    create sdl window

--- src/GameWindow.cpp
+++ src/GameWindow.cpp
@@ -1,26 +1,60 @@
 #include "GameWindow.h"
 
 #include <SDL.h>
+#include <SDL_log.h>
+#include <SDL_assert.h>
 
-struct GameWindow
-{
-    SDL_Window* window;
-};
-
-GameWindow* init_game_window()
+GameWindow* game_window_init()
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) < 0)
     {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "error initing SDL: %s", SDL_GetError());
         return nullptr;
     }
 
     GameWindow* game_window = new GameWindow();
+
+    game_window->window = SDL_CreateWindow(
+        "Vulkan Rainy Street Demo",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        1280, 720,
+        SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (game_window->window == nullptr)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "failed to create SDL window: %s", SDL_GetError());
+        return nullptr;
+    }
+
     return game_window;
 }
 
-void destory_game_window(GameWindow* game_window)
+void game_window_destory(GameWindow* game_window)
 {
     SDL_assert(game_window);
 
+    if (game_window->window != nullptr)
+    {
+        SDL_DestroyWindow(game_window->window);
+    }
+
     delete game_window;
+
+    SDL_Quit();
+}
+
+void game_window_process_events(GameWindow* game_window)
+{
+    SDL_Event sdl_event;
+    while (SDL_PollEvent(&sdl_event) != 0)
+    {
+        if (sdl_event.type == SDL_QUIT)
+        {
+            game_window->quit_requested = true;
+        }
+    }
+}
+
+bool game_window_is_quit_requested(GameWindow* game_window)
+{
+    return game_window->quit_requested;
 }
--- src/GameWindow.h
+++ src/GameWindow.h
@@ -1,10 +1,18 @@
 #ifndef VRSD_GameWindow_h_
 #define VRSD_GameWindow_h_
 
-struct GameWindow;
+struct SDL_Window;
 
-GameWindow* init_game_window();
-void destory_game_window(GameWindow* game_window);
+struct GameWindow
+{
+    SDL_Window* window = nullptr;
+    bool quit_requested = false;
+};
+
+GameWindow* game_window_init();
+void game_window_destory(GameWindow* game_window);
+
+void game_window_process_events(GameWindow* game_window);
+bool game_window_is_quit_requested(GameWindow* game_window);
 
 #endif // VRSD_GameWindow_h_
-
